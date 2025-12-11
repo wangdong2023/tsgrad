1fd314374dbb44c7a53a4b8ef4790c400a70f2c4
karl <karl@example.com>
2025-12-11 23:11:35 +0800

    test backward

--- index.ts
+++ index.ts
@@ -1,17 +1,39 @@
 
-export class Value {
+
+function topoSort(val: Value, seen: Set<Value>): Value[] {
+    let out: Value[] = [];
+
+    for (const child of val.prev) {
+        if (seen.has(child)) {
+            continue;
+        }
+        out = out.concat(topoSort(child, seen));
+        seen.add(child);
+    }
+
+    out.push(val);
+
+    return out;
+}
+
+
+export class Value { // Kapathy implements more, but I'm lazy, this should do
     public grad: number = 0;
     public _backward: () => void = () => {};
 
     constructor(
         public data: number,
         public prev: Value[] =[],
-        public label: string = '',
-        public ops: string = '') {
+        public ops: string = '',
+        public label: string = '_') {
     }
 
-    add(other: Value): Value {
-        let out = new Value(this.data + other.data, [this, other], '', '+');
+    add(other: Value | number): Value {
+        if (typeof other === 'number') {
+            other = new Value(other);
+        }
+
+        let out = new Value(this.data + other.data, [this, other], '+', '_');
         this._backward = () => {
             this.grad += out.grad;
             other.grad += out.grad;
@@ -19,8 +41,11 @@ export class Value {
         return out;
     }
 
-    mul(other: Value): Value {
-        let out = new Value(this.data * other.data, [this, other], '', '*');
+    mul(other: Value | number): Value {
+        if (typeof other === 'number') {
+            other = new Value(other);
+        }
+        let out = new Value(this.data * other.data, [this, other], '*', '_');
         this._backward = () => {
             // console.log(out, this, other);
             this.grad += out.grad * other.data;
@@ -30,12 +55,34 @@ export class Value {
         return out;
     }
 
-    toString(): string {
-        return `${this.label}(${this.data}, ${this.grad})`
+    tanh(): Value {
+        let ev = Math.pow(Math.E, 2 * this.data);
+        let out = new Value((ev - 1) / (ev + 1), [this], 'tanh', '_');
+        this._backward = () => {
+            this.grad += out.grad * (1 - Math.pow(out.data, 2));
+        }
+        return out;
+    }
+
+    toString(precision: number=3): string {
+        return `${this.label}(${this.data.toPrecision(precision)}, ${this.grad.toPrecision(precision)})`
+    }
+
+    backward() {
+        let params = topoSort(this, new Set([]));
+        params.reverse();
+        for (const param of params) {
+            param._backward();
+        }
+
     }
 };
 
 
-
+let x = new Value(2);
+let o = x.tanh();
+o.grad = 1;
+x._backward()
+console.log(x.toString());
 
 
--- test.ts
+++ test.ts
@@ -7,10 +7,11 @@ export function assert(condition: unknown, msg?: string): asserts condition {
 }
 
 function test_basic() {
-    let x = new Value(2, [], 'x');
+    let x = new Value(1, [], 'x');
+    let x1 = x.add(1);
     let y = new Value(3, [], 'y');
     let b = new Value(1, [], 'b');
-    let xy = x.mul(y);
+    let xy = x1.mul(y);
     let z = xy.add(b); // z = x * y + b
     z.label = 'z';
 
@@ -19,6 +20,7 @@ function test_basic() {
 
     b._backward();
     xy._backward();
+    x1._backward();
 
     x._backward();
 
@@ -30,4 +32,26 @@ function test_basic() {
     console.log("test_basic passed!")
 }
 
+
+function test_backward() {
+    let x = new Value(1, [], 'x');
+    let x1 = x.add(1);
+    let y = new Value(3, [], 'y');
+    let b = new Value(1, [], 'b');
+    let xy = x1.mul(y);
+    let z = xy.add(b); // z = x * y + b
+    z.label = 'z';
+
+    z.grad = 1;
+    z.backward()
+
+    assert(z.grad == 1);
+    assert(b.grad == 1);
+    assert(xy.grad == 1);
+    assert(x.grad == 3);
+    assert(y.grad == 2);
+    console.log("test_basic passed!")
+}
+
 test_basic();
+test_backward();
